d977c290aa51256b3025da7bc02c1ac93213a3c0
Dominic Meyer <dominic.meyer2000@gmail.com>
2025-03-02 14:13:47 +0000

    Added button

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Tanuki-Ledger")
 
+	w.SetContent(createAddDatabaseButton())
+
 	w.ShowAndRun()
+}
+
+func createAddDatabaseButton() *widget.Button {
+	button := widget.NewButton("Add Database Connection", func() {})
+
+	return button
 }
