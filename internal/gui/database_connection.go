1791675571abde61cd36e2942c36ddf71b3f7f32
Dominic Meyer <dominic.meyer2000@gmail.com>
2025-03-04 15:59:40 +0000

    Added Form to connection to a database

--- internal/gui/database_connection.go
+++ internal/gui/database_connection.go
@@ -1,10 +1,46 @@
 package gui
 
-import "fyne.io/fyne/v2/widget"
+import (
+	"fyne.io/fyne/v2/widget"
+)
 
 func NewDatabaseConnectionButton() *widget.Button {
 	return widget.NewButton(
 		"Add Database Connection",
-		func() {},
+		nil,
 	)
+}
+
+type DatabaseConnectionForm struct {
+	*widget.Form
+	databaseURLInput      *widget.Entry
+	databaseUsernameInput *widget.Entry
+	databasePasswordInput *widget.Entry
+	databaseNameInput     *widget.Entry
+}
+
+func NewDatabaseConnectionForm() *DatabaseConnectionForm {
+	databaseURLInput := widget.NewEntry()
+	databaseUsernameInput := widget.NewEntry()
+	databasePasswordInput := widget.NewEntry()
+	databaseNameInput := widget.NewEntry()
+
+	form := widget.NewForm(
+		widget.NewFormItem("url", databaseURLInput),
+		widget.NewFormItem("username", databaseUsernameInput),
+		widget.NewFormItem("password", databasePasswordInput),
+		widget.NewFormItem("database name", databaseNameInput),
+	)
+	form.CancelText = "Cancel"
+	form.SubmitText = "Connect"
+
+	databaseConnectionForm := DatabaseConnectionForm{
+		form,
+		databaseURLInput,
+		databaseUsernameInput,
+		databasePasswordInput,
+		databaseNameInput,
+	}
+
+	return &databaseConnectionForm
 }
--- internal/gui/database_connection_test.go
+++ internal/gui/database_connection_test.go
@@ -1,22 +1,65 @@
-package gui_test
+package gui
 
 import (
 	"testing"
 
-	"github.com/dominicmeyer/Tanuki-Ledger/internal/gui"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestDatabaseConnectionButton_NotNil(t *testing.T) {
-	t.Parallel()
-
-	button := gui.NewDatabaseConnectionButton()
-	assert.NotNil(t, button, "Button shouldn't be nil")
-}
 
 func TestDatabaseConnectionButton_Text(t *testing.T) {
 	t.Parallel()
 
-	button := gui.NewDatabaseConnectionButton()
+	button := NewDatabaseConnectionButton()
+
 	assert.Equal(t, "Add Database Connection", button.Text, "Button Text wasn't right")
+}
+
+func TestDatabaseConnectionDialog_ItemsCount(t *testing.T) {
+	t.Parallel()
+
+	form := NewDatabaseConnectionForm()
+
+	assert.Len(t, form.Items, 4, "Form should have four elements")
+}
+
+func TestDatabaseConnectionDialog_ItemsContainStructInputs(t *testing.T) {
+	t.Parallel()
+
+	form := NewDatabaseConnectionForm()
+
+	assert.Equal(
+		t,
+		form.databaseURLInput,
+		form.Items[0].Widget,
+		"databaseURLInput is not the same as in the base form",
+	)
+	assert.Equal(
+		t,
+		form.databaseUsernameInput,
+		form.Items[1].Widget,
+		"databaseUsernameInput is not the same as in the base form",
+	)
+	assert.Equal(
+		t,
+		form.databasePasswordInput,
+		form.Items[2].Widget,
+		"databasePasswordInput is not the same as in the base form",
+	)
+	assert.Equal(
+		t,
+		form.databaseNameInput,
+		form.Items[3].Widget,
+		"databaseNameInput is not the same as in the base form",
+	)
+}
+
+func TestDatabaseConnectionDialog_ItemsText(t *testing.T) {
+	t.Parallel()
+
+	form := NewDatabaseConnectionForm()
+
+	assert.Equal(t, "url", form.Items[0].Text, "url text wasn't right")
+	assert.Equal(t, "username", form.Items[1].Text, "username text wasn't right")
+	assert.Equal(t, "password", form.Items[2].Text, "password text wasn't right")
+	assert.Equal(t, "database name", form.Items[3].Text, "database name text wasn't right")
 }
