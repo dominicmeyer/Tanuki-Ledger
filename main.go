9fdc6669a6b425e7bae6272e0894284293be42e9
Dominic Meyer <dominic.meyer2000@gmail.com>
2025-03-17 10:36:30 +0000

    Added setup for pipeline to access port

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	timeoutSeconds := 3
+
+	server := new(http.Server)
+	server.Addr = "localhost:8080"
+	server.ReadHeaderTimeout = time.Duration(timeoutSeconds) * time.Second
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
